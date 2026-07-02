18e5d4957ba34a7a21730e9868bd4eb6b5c98216
Renne Rocha <renne@rocha.dev.br>
2026-07-02 14:56:10 -0300

    Removendo código morto

--- brutils/cnpj.py
+++ brutils/cnpj.py
@@ -94,8 +94,6 @@ def display(cnpj: str) -> str | None:
         cnpj[:2], cnpj[2:5], cnpj[5:8], cnpj[8:12], cnpj[12:]
     )
 
-    return all(char in (digits + ascii_uppercase) for char in cnpj)
-
 
 def format_cnpj(cnpj: str) -> str | None:
     """
